fcf3e193bf275e2fce9981475fcfc7206c923b20
jmaffa <90526195+jmaffa@users.noreply.github.com>
2025-02-27 15:33:24 -0500

    setup routing

--- srch-website/src/App.jsx
+++ srch-website/src/App.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// import { Home } from './pages/Home.jsx'
 import './App.css'
+import Home from './pages/Home'
+import Docs from "./pages/Docs.jsx";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is the entry point!</h1>
-    </>
-  )
+    <Router basename="/srch-s25/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/docs" element={<Docs />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
