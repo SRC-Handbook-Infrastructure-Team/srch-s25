88e0af790199552b474f297513acb24108bbf625
jmaffa <90526195+jmaffa@users.noreply.github.com>
2025-02-28 01:33:24 -0500

    navbar with hierarchy

--- srch-website/src/components/NavBar.jsx
+++ srch-website/src/components/NavBar.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import { NavigationMenu } from 'radix-ui'
-import { Link } from "react-router-dom"
-import './NavBar.css'
+import React from "react";
+import { NavigationMenu } from "radix-ui";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <NavigationMenu.Root className='navigation-menu'>
-      <NavigationMenu.List className='navigation-menu-list'>
+    <NavigationMenu.Root className="navigation-menu">
+      <NavigationMenu.List className="navigation-menu-list">
         <NavigationMenu.Item>
-          <NavigationMenu.Link>
+          <NavigationMenu.Link asChild>
             <Link to="/">Home</Link>
           </NavigationMenu.Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item>
-          <NavigationMenu.Link>
-            <Link to="/docs">Docs</Link>
+          {/* 1. Heading */}
+          <NavigationMenu.Link asChild>
+            <Link to="/">1. Fairness and Justice</Link>
+          </NavigationMenu.Link>
+          {/* sub menus of 1. */}
+          <NavigationMenu.List className="navigation-menu-list">
+            <NavigationMenu.Item>
+              <NavigationMenu.Link asChild>
+                <Link to="/">2.c.i. -- Abbrv</Link>
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item>
+              <NavigationMenu.Link asChild>
+                <Link to="/">2.c.ii. - Abbrv</Link>
+              </NavigationMenu.Link>
+              <NavigationMenu.List className="navigation-menu-list">
+                <NavigationMenu.Item>
+                  <NavigationMenu.Link asChild>
+                    <Link to="/">Appendix</Link>
+                  </NavigationMenu.Link>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item></NavigationMenu.Item>
+              </NavigationMenu.List>
+            </NavigationMenu.Item>
+          </NavigationMenu.List>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link asChild>
+            <Link to="/">2. Data and AI: Accuracy, Fairness, and Justice</Link>
           </NavigationMenu.Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
@@ -22,4 +49,4 @@ function NavBar() {
   );
 }
 
-export default NavBar
+export default NavBar;
