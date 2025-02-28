7a6145732a75fcedeac74a2f70fcbcddb7f3a4af
Christina Peng <christina_peng@brown.edu>
2025-02-27 19:42:37 -0500

    testing mui persistent drawer

--- srch-website/src/App.jsx
+++ srch-website/src/App.jsx
@@ -2,23 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Demo from './components/Demo.jsx'
+import Drawer from "./components/Drawer.tsx";
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <Demo/>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is the entry point!</h1>
-    </>
-  )
+    <Drawer />
+  );
 }
 
 export default App
