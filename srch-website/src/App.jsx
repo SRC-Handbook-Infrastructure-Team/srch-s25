7513c6a1a1c3ca77f2cb119f68beaa567939d20b
jmaffa <90526195+jmaffa@users.noreply.github.com>
2025-03-14 10:34:50 -0400

    general routing

--- srch-website/src/App.jsx
+++ srch-website/src/App.jsx
@@ -3,20 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Newpage from './pages/Newpage'
+
+
+
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is the entry point!</h1>
-    </>
-  )
+    <ChakraProvider>
+      <BrowserRouter basename="/srch-s25/">
+        {/* <Home */}
+        {/* TODO: add more paths for everything */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Newpage />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
 
 export default App
