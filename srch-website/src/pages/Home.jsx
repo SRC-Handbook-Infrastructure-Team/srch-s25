1fe752be6b38b6e5e139b29e03cb10b06153750b
A.J. Shulman <Shulman.aj@gmail.com>
2025-03-31 17:22:50 -0400

    feat: added custom rendering for Markdown components

--- srch-website/src/pages/Home.jsx
+++ srch-website/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import MarkdownRenderer from '../util/MarkdownRenderer'
-
+import testMD from '../markdown/test.md?raw'
 
 function Home() {
   return (
     <div>
         <h1>This is the home page</h1>
-        <MarkdownRenderer>
-        </MarkdownRenderer>
+        <MarkdownRenderer content={testMD} />
         {/* TODO: Generally we want to show either the currently selected file (using markdown renderer) */}
         {/* Or the actual homepage (can just be a component here) */}
     </div>
