c9bcd023241675660b4bcc2ce5f600d5c87570fc
A.J. Shulman <Shulman.aj@gmail.com>
2025-03-31 17:23:06 -0400

    Revert "feat: added custom rendering for Markdown components"
    This reverts commit 1fe752be6b38b6e5e139b29e03cb10b06153750b.

--- srch-website/src/pages/Home.jsx
+++ srch-website/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MarkdownRenderer from '../util/MarkdownRenderer'
-import testMD from '../markdown/test.md?raw'
+
 
 function Home() {
   return (
     <div>
         <h1>This is the home page</h1>
-        <MarkdownRenderer content={testMD} />
+        <MarkdownRenderer>
+        </MarkdownRenderer>
         {/* TODO: Generally we want to show either the currently selected file (using markdown renderer) */}
         {/* Or the actual homepage (can just be a component here) */}
     </div>
--- srch-website/src/util/MarkdownRenderer.jsx
+++ srch-website/src/util/MarkdownRenderer.jsx
@@ -1,88 +1,33 @@
 import {useMemo} from 'react'
 import ReactMarkdown from 'react-markdown'
-import rehypeRaw from 'rehype-raw'
 import {
     Text,
-    Heading,
-    Link,
-    UnorderedList,
-    OrderedList,
-    ListItem,
-    Box,
-    Code,
-    Divider
+    Heading
 } from '@chakra-ui/react'
 
-// TODO: separate "main.MD" files from "drawer.MD" files
-function MarkdownRenderer({ content }) {
-    // If no content is provided, use empty string
-    const markdownContent = content || '';
-    
-    // Component overrides for markdown elements
-    const components = useMemo(() => ({
-        h1: (props) => <Heading as="h1" size="xl" mt={6} mb={4} fontWeight="bold" {...props} />,
-        h2: (props) => <Heading as="h2" size="lg" mt={5} mb={3} fontWeight="semibold" {...props} />,
-        h3: (props) => <Heading as="h3" size="md" mt={4} mb={2} fontWeight="medium" {...props} />,
-        h4: (props) => <Heading as="h4" size="sm" mt={3} mb={2} fontWeight="medium" {...props} />,
-        p: (props) => <Text mb={4} fontSize="md" lineHeight="1.7" {...props} />,
-        a: (props) => <Link color="blue.500" textDecoration="underline" isExternal {...props} />,
-        ul: (props) => <UnorderedList pl={4} mb={4} spacing={2} {...props} />,
-        ol: (props) => <OrderedList pl={4} mb={4} spacing={2} {...props} />,
-        li: (props) => <ListItem fontSize="md" {...props} />,
-        blockquote: (props) => (
-            <Box 
-                borderLeftWidth="4px" 
-                borderLeftColor="gray.200" 
-                pl={4} 
-                py={2} 
-                my={4} 
-                bg="gray.50"
-                {...props} 
-            />
-        ),
-        code: ({ inline, ...props }) => {
-            if (inline) {
-                return <Code px={2} py={1} borderRadius="md" bg="gray.100" {...props} />;
-            }
-            return (
-                <Box 
-                    as="pre" 
-                    p={4} 
-                    my={4} 
-                    borderRadius="md" 
-                    bg="gray.100" 
-                    overflowX="auto"
-                    whiteSpace="pre"
-                    fontSize="sm"
-                    {...props}
-                />
-            );
-        },
-        hr: () => <Divider my={6} borderColor="gray.300" />,
-        strong: (props) => <Text as="strong" fontWeight="bold" {...props} />,
-        em: (props) => <Text as="em" fontStyle="italic" {...props} />,
-        img: (props) => (
-            <Box my={4}>
-                <img style={{ maxWidth: '100%' }} {...props} alt={props.alt || ''} />
-            </Box>
-        ),
-        table: (props) => (
-            <Box overflowX="auto" my={4}>
-                <Box as="table" width="full" {...props} />
-            </Box>
-        ),
-        th: (props) => <Box as="th" p={2} borderWidth="1px" fontWeight="bold" {...props} />,
-        td: (props) => <Box as="td" p={2} borderWidth="1px" {...props} />
-    }))
+import testMD from '../markdown/test.md?raw'
 
-    return (
-        <ReactMarkdown
-            components={components}
-            rehypePlugins={[rehypeRaw]}
-        >
-            {markdownContent}
-        </ReactMarkdown>
-    )
+
+// TODO: importing MD file as a raw string, improve the extensibility of it
+// TODO: separate "main.MD" files from "drawer.MD" files
+function MarkdownRenderer( content ) {
+    console.log(testMD)
+    //  TODO: Component override
+    const components =  useMemo(() => ({
+        h1: (props) => <Heading size="lg" {...props}/>,
+        h2: (props) => <Heading size="md" {...props}/>,
+        p: (props) => <Text {...props}/>
+    }))
+  return (
+    <ReactMarkdown
+        // TODO: This is our component 
+        components = {components}
+    >
+        {testMD}
+    </ReactMarkdown>
+    // <div>MarkdownRenderer</div>
+    
+  )
 }
 
 export default MarkdownRenderer
