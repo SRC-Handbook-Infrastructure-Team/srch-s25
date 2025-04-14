81b8c7f87a233e8a453d031c515b598e1ffa3cfa
sgsaab <sana_saab@brown.edu>
2025-04-13 20:36:24 -0400

    Updates

---
+++ assets/0_home-DHxWzxra.js
@@ -0,0 +1,3 @@
+const e=`# SRC Handbook\r
+\r
+This is the home page of the project`;export{e as default};
---
+++ assets/2_data-ai-Gp2nliWS.js
@@ -0,0 +1,11 @@
+const e=`# Data & AI\r
+\r
+## Subsection 1\r
+\r
+Here's a few sentences about Data and AI\r
+\r
+## Subsection 2\r
+\r
+And here is some supporting evidence! Does there just need to be a lot of words. Does there just need to be a lot of words.  Does there just need to be a lot of words.  Does there just need to be a lot of words. \r
+\r
+`;export{e as default};
