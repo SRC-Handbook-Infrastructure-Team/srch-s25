c0b0c3d15cd294e5cf26f0a9880ebe5b11cdc27a
jmaffa <90526195+jmaffa@users.noreply.github.com>
2025-04-15 10:51:29 -0400

    Add fallback for routing

--- srch-website/vite.config.js
+++ srch-website/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-  base: '/srch-s25/'
-})
+  base: "/srch-s25/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        fallback: resolve(__dirname, "index.html"), // this creates 404.html
+      },
+    },
+  },
+});
